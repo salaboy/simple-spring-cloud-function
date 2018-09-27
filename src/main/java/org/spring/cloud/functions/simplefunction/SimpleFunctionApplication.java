e290f7eba513d89113219ba7038d2b2aff4d5443
salaboy <salaboy@gmail.com>
2018-09-27 15:43:33 -0400

    initial commit

---
+++ src/main/java/org/spring/cloud/functions/simplefunction/SimpleFunctionApplication.java
@@ -0,0 +1,21 @@
+package org.spring.cloud.functions.simplefunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class SimpleFunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleFunctionApplication.class, args);
+    }
+
+    @Bean
+    public Function<Flux<String>, Flux<String>> uppercase() {
+        return flux -> flux.map(value -> value.toUpperCase());
+    }
+}
